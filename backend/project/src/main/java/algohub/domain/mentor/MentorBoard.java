a71936c521b259d9f37dda59a2ceebc71d345a27
배준수 <comply95@gmail.com>
2021-05-23 17:45:47 +0900

    5.23 멘토 멘티 기능 추가

---
+++ backend/project/src/main/java/algohub/domain/mentor/MentorBoard.java
@@ -0,0 +1,13 @@
+package algohub.domain.mentor;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MentorBoard {
+    private int mb_c_id;
+    private String mb_title;
+    private String mb_content;
+    private String mb_date;
+}
