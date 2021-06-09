14ef3f301a34cf14c1c6079f3603f85fd42079d0
20150650 <ckdtjd411@kumoh.ac.kr>
2021-06-09 12:09:08 +0900

    21.06.09 화면공유 추가

--- frontend/src/components/mentoring/chatting/Room.js
+++ frontend/src/components/mentoring/chatting/Room.js
@@ -5,22 +5,41 @@ import '../../../styles/Room.css'
 import axios from 'axios'
 
 export default function Room(props) {
+  // user variables
+  const certificate = useCertificate()
+  const [localUserName, setLocalUserName] = useState()
+  const [mentorName, setMentorName] = useState('')
+  const remoteUser = useRef()
+
+  // room variables
   const [value, setValue] = useState('')
   const [chatLog, setChatLog] = useState([])
-  const [stream, setStream] = useState()
-  const [localUserName, setLocalUserName] = useState()
-  const certificate = useCertificate()
+  let localRoom = props.location.state.chatId
+
+  // RTC variabels
   const socket = useRef()
-  const localVideo = useRef()
-  const remoteVideo = useRef()
-  const senders = useRef()
+  // const [stream, setStream] = useState()
   const myPeerConnection = useRef()
   const dataChannel = useRef()
-  let localRoom = props.location.state.chatId;
+
+  // video variables
+  const localVideo = useRef()
+  const localStream = useRef()
+  const remoteVideo = useRef()
+  const senders = useRef([])
+
+  // configurations
   const mediaConstraints = {
     audio: false,
     video: {
-      width: 460,
+      width: 480,
+      height: 360
+    }
+  }
+  const screenConstraints = {
+    video: {
+      cursor: "always",
+      width: 480,
       height: 360
     }
   }
@@ -30,59 +49,63 @@ export default function Room(props) {
       { 'urls': 'stun:stun.l.google.com:19302' },
     ]
   }
-  useEffect(() => {
-    if (certificate.localUserName) {
-      socket.current = new WebSocket("ws://localhost:8080/signal")
 
-      setStream(stream)
+  useEffect(() => {
+    const mentorName = window.location.href.split('/')[5]
+    setMentorName(mentorName)
+    if (certificate.localUserName) {
       setLocalUserName(certificate.localUserName)
-      if (localVideo.current) {
-        localVideo.current.srcObject = stream
-      }
     }
-    if (localUserName) { initSocket() }
+    if (localUserName) {
+      socket.current = new WebSocket("ws://localhost:8080/signal")
+      initSocket()
+    }
   }, [certificate.loading, localUserName])
 
+  // init
   function initSocket() {
+    // add an event listener to get to know when a connection is open
     socket.current.onopen = function () {
       sendToServer({
         from: localUserName,
         type: 'join',
         data: localRoom
-      });
+      })
     }
+    // add an event listener for a message being received
     socket.current.onmessage = function (msg) {
       let message = JSON.parse(msg.data);
       switch (message.type) {
         case "text":
-          console.log('Text message from ' + message.from + ' received: ' + message.data);
-          break;
+          console.log('Text message from ' + message.from + ' received: ' + message.data)
+          break
         case "offer":
-          console.log('Signal OFFER received');
-          handleOfferMessage(message);
-          break;
+          console.log('Signal OFFER received')
+          handleOfferMessage(message)
+          break
         case "answer":
-          console.log('Signal ANSWER received');
-          handleAnswerMessage(message);
-          break;
+          console.log('Signal ANSWER received')
+          handleAnswerMessage(message)
+          break
         case "ice":
-          console.log('Signal ICE Candidate received');
-          handleNewICECandidateMessage(message);
-          break;
+          console.log('Signal ICE Candidate received')
+          handleNewICECandidateMessage(message)
+          break
         case "join":
           console.log('Signal JOIN received')
-          handlePeerConnection(message);
-          break;
+          handlePeerConnection(message)
+          break
         default:
-          handleErrorMessage('Wrong type message received from server');
+          handleErrorMessage('Wrong type message received from server')
       }
     }
+    // a listener for the socket being closed event
     socket.current.onclose = function (message) {
-      console.log('Socket has been closed');
+      console.log('Socket has been closed')
     };
     // an event listener to handle socket errors
     socket.current.onerror = function (message) {
-      handleErrorMessage("Error: " + message);
+      handleErrorMessage("Error: " + message)
     }
   }
 
@@ -131,35 +154,24 @@ export default function Room(props) {
   }
 
   function handleOfferMessage(message) {
-    console.log('Accepting Offer Message');
-    let desc = new RTCSessionDescription(message.sdp);
-
+    console.log('Accepting Offer Message')
+    let desc = new RTCSessionDescription(message.sdp)
     if (desc != null && message.sdp != null) {
-      console.log('RTC Signalling state: ' + myPeerConnection.current.signalingState);
+      console.log('RTC Signalling state: ' + myPeerConnection.current.signalingState)
       myPeerConnection.current.setRemoteDescription(desc).then(function () {
-        console.log("Set up local media stream");
-        return navigator.mediaDevices.getUserMedia(mediaConstraints);
-      }).then(function (stream) {
-        console.log("-- Local video stream obtained");
-        // localStream = stream;
-        try {
-          localVideo.current.srcObject = stream;
-        } catch (error) {
-          localVideo.src = window.URL.createObjectURL(stream);
-        }
-        console.log("-- Adding stream to the RTCPeerConnection");
-        stream.getTracks().forEach(track => myPeerConnection.current.addTrack(track, stream));
+        console.log("Set up local media stream")
+        return navigator.mediaDevices.getUserMedia(mediaConstraints)
       })
-        .then(function () {
-          console.log("-- Creating answer");
-          return myPeerConnection.current.createAnswer();
+        .then(() => {
+          console.log("-- Creating answer")
+          return myPeerConnection.current.createAnswer()
         })
-        .then(function (answer) {
-          console.log("-- Setting local description after creating answer");
-          return myPeerConnection.current.setLocalDescription(answer);
+        .then((answer) => {
+          console.log("-- Setting local description after creating answer")
+          return myPeerConnection.current.setLocalDescription(answer)
         })
-        .then(function () {
-          console.log("Sending answer packet back to other peer");
+        .then(() => {
+          console.log("Sending answer packet back to other peer")
           sendToServer({
             from: localUserName,
             type: 'answer',
@@ -171,16 +183,17 @@ export default function Room(props) {
   }
 
   function handleAnswerMessage(message) {
-    console.log("The peer has accepted request");
-    myPeerConnection.current.setRemoteDescription(message.sdp).catch(handleErrorMessage);
+    console.log("The peer has accepted request")
+    myPeerConnection.current.setRemoteDescription(message.sdp).catch(handleErrorMessage)
   }
 
   function handleNewICECandidateMessage(message) {
-    let candidate = new RTCIceCandidate(message.candidate);
-    console.log("Adding received ICE candidate: " + JSON.stringify(candidate));
-    myPeerConnection.current.addIceCandidate(candidate).catch(handleErrorMessage);
+    let candidate = new RTCIceCandidate(message.candidate)
+    console.log("Adding received ICE candidate: " + JSON.stringify(candidate))
+    myPeerConnection.current.addIceCandidate(candidate).catch(handleErrorMessage)
   }
 
+  // create peer connection, get media, start negotiating when second participant appears
   function handlePeerConnection(message) {
     createPeerConnection()
     getMedia(mediaConstraints)
@@ -188,64 +201,67 @@ export default function Room(props) {
       myPeerConnection.current.onnegotiationneeded = handleNegotiationNeededEvent
     }
   }
+
+  // initialize media stream
   function getMedia(constraints) {
-    navigator.mediaDevices.getUserMedia(constraints).then(mediaStream => {
-      localVideo.current.srcObject = mediaStream;
-      mediaStream.getTracks().forEach(track => {
-        myPeerConnection.current.addTrack(track, mediaStream);
-      });
+    navigator.mediaDevices.getUserMedia(constraints).then(stream => {
+      localVideo.current.srcObject = stream
+      localStream.current = stream
+      localStream.current.getTracks().forEach(track => senders.current.push(myPeerConnection.current.addTrack(track, localStream.current)))
     })
   }
+
+  // start screen sharing
   function shareScreen() {
-    navigator.mediaDevices.getDisplayMedis({ cursor: true }).then(stream => {
+    navigator.mediaDevices.getDisplayMedia(screenConstraints).then(stream => {
+      localVideo.current.srcObject = stream
       const screenTrack = stream.getTracks()[0]
       senders.current.find(sender => sender.track.kind === 'video').replaceTrack(screenTrack)
-      screenTrack.onended = function () {
-        senders.current.find(sender => sender.track.kind === 'video').replaceTrack(localVideo.current.getTracks()[1])
+      screenTrack.onended = function (e) {
+        localVideo.current.srcObject = localStream.current
+        senders.current.find(sender => sender.track.kind === 'video').replaceTrack(localStream.current.getTracks()[0])
       }
     })
   }
   function createPeerConnection() {
-    myPeerConnection.current = new RTCPeerConnection(peerConnectionConfig);
+    myPeerConnection.current = new RTCPeerConnection(peerConnectionConfig)
     myPeerConnection.current.onicecandidate = handleICECandidateEvent
-    myPeerConnection.current.ontrack = handleTrackEvent;
+    myPeerConnection.current.ontrack = handleTrackEvent
 
-    dataChannel.current = myPeerConnection.current.createDataChannel("dataChannel");
+    dataChannel.current = myPeerConnection.current.createDataChannel("dataChannel")
     dataChannel.current.onerror = function (error) {
-      console.log("Error occured on datachannel:", error);
-    };
+      console.log("Error occured on datachannel:", error)
+    }
     dataChannel.current.onmessage = handleReceiveMessage
     dataChannel.current.onclose = function () {
-      console.log("data channel is closed");
-    };
+      console.log("data channel is closed")
+    }
     myPeerConnection.current.ondatachannel = function (event) {
       dataChannel.current = event.channel;
-    };
+    }
   }
   function handleReceiveMessage(e) {
     const msg = JSON.parse(e.data)
     setChatLog(chatLog => [...chatLog, { username: msg.username, content: msg.content }])
   }
 
+  // WebRTC called handler to begin ICE negotiation
   function handleNegotiationNeededEvent() {
-    console.log('createpeer')
-    myPeerConnection.current.createOffer().then(function (offer) {
+    myPeerConnection.current.createOffer().then((offer) => {
       return myPeerConnection.current.setLocalDescription(offer)
+    }).then(() => {
+      sendToServer({
+        from: localUserName,
+        type: 'offer',
+        sdp: myPeerConnection.current.localDescription
+      })
+      console.log('Negotiation Needed Event: SDP offer sent')
+    }).catch((error) => {
+      handleErrorMessage('failure to connect error: ', error)
     })
-      .then(function () {
-        sendToServer({
-          from: localUserName,
-          type: 'offer',
-          sdp: myPeerConnection.current.localDescription
-        })
-        console.log('Negotiation Needed Event: SDP offer sent')
-      })
-      .catch(function (reason) {
-        // an error occurred, so handle the failure to connect
-        handleErrorMessage('failure to connect error: ', reason)
-      })
   }
 
+  // send ICE candidate to the peer through the server
   function handleICECandidateEvent(event) {
     if (event.candidate) {
       sendToServer({
@@ -253,12 +269,12 @@ export default function Room(props) {
         type: 'ice',
         candidate: event.candidate
       });
-      console.log('ICE Candidate Event: ICE candidate sent');
+      console.log('ICE Candidate Event: ICE candidate sent')
     }
   }
 
-  function handleTrackEvent(event) {
-    console.log(event)
+  async function handleTrackEvent(event) {
+    console.log('remote video connected')
     remoteVideo.current.srcObject = event.streams[0];
   }
 
@@ -266,23 +282,10 @@ export default function Room(props) {
     console.log(error)
   }
 
+  // use JSON format to send WebSocket message
   function sendToServer(msg) {
     let msgJSON = JSON.stringify(msg)
     socket.current.send(msgJSON)
-  }
-
-  function handleDoubleClick(e) {
-    e.target.requestFullscreen({ navigationUI: "hide" })
-  }
-
-  function shareScreen() {
-    navigator.mediaDevices.getDisplayMedia(mediaConstraints).then(stream =>
-      localVideo.current.srcObject = stream
-    )
-  }
-
-  function handleOnChange(e) {
-    setValue(e.target.value)
   }
 
   function handleOnClick() {
@@ -291,24 +294,25 @@ export default function Room(props) {
       username: localUserName,
       content: value
     }
-    setChatLog(chatLog => [...chatLog, { username: msg.username, content: msg.content }])
     dataChannel.current.send(JSON.stringify(msg))
+    setChatLog(chatLog => [...chatLog, { username: msg.username, content: msg.content }])
     setValue('')
   }
 
+  function handleDoubleClick(e) {
+    e.target.requestFullscreen({ navigationUI: "hide" })
+  }
 
   function exitRoom() {
     let url = props.history.location.pathname.split('/')
     url.pop(0)
     url = url.join('/')
-    // stop()
-    return axios.post(`/api/mentors/exitRoom/${localUserName}/${localRoom}`).then((res) => {
-      console.log(props.history.location)
-      if (res.data.message === 'OK') {
-        alert('채팅방이 종료되었습니다.')
-      }
-      props.history.push(url);
-    })
+    if (window.confirm("정말 나가시겠습니까?")) {
+      stop()
+      return axios.post(`/api/mentors/exitRoom/${localUserName}/${localRoom}`).then((res) => {
+        props.history.push(url);
+      })
+    }
   }
 
   return (
