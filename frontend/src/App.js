7b52db37e2c9e40ba48becfdad91c13fc2f30890
lwslwslws <sitpo96@naver.com>
2021-06-12 20:58:48 +0900

    홈 화면 알고리즘 화면으로 교체

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,16 @@
 import "./App.css";
 import React from "react";
-import Home from "./routes/Home";
 import Algorithm from "./routes/Algorithm";
 import Mentoring from "./routes/Mentoring";
 import LoginRegister from "./routes/LoginRegister";
-import Rank from "./routes/Rank";
-import Board from "./routes/Board";
 import Room from "./components/mentoring/chatting/Room";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={Home} />
           <Route
             exact
             path="/mentoring/mentiview/:m_name/:room"
@@ -23,8 +19,7 @@ class App extends React.Component {
           <Route path="/auth" component={LoginRegister} />
           <Route path="/category" component={Algorithm} />
           <Route path="/mentoring" component={Mentoring} />
-          <Route path="/rank" component={Rank} />
-          <Route path="/board" component={Board} />
+          <Redirect path="*" to="/category" />
         </Switch>
       </BrowserRouter>
     );
